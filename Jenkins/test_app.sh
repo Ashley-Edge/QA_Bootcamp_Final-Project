0cc510bad1f7a146f348bb059974dba276da8cc0
Mohammed Sayeed <abusayeed16@gmail.com>
2021-03-22 11:58:45 +0000

    Change directory

--- Jenkins/test_app.sh
+++ Jenkins/test_app.sh
@@ -1,9 +1,12 @@
 #Get packages
-sudo apt update
-sudo apt upgrade -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 #Clone into repo
 https://github.com/Team-CodeVid/spring-petclinic-rest
+
+#Enter directory
+cd spring-petclinic-rest
 
 #Install Java, OpenJDK, Maven
 sudo apt install openjdk-8-jre maven default-jre -y
