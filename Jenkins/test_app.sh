d7bb770c11a75aaf5c29785b71f715148f95565a
Mohammed Sayeed <abusayeed16@gmail.com>
2021-03-22 11:21:45 +0000

    Cloning into repo

--- Jenkins/test_app.sh
+++ Jenkins/test_app.sh
@@ -2,6 +2,9 @@
 sudo apt update
 sudo apt upgrade -y
 
+#Clone into repo
+https://github.com/Team-CodeVid/spring-petclinic-rest
+
 #Install Java, OpenJDK, Maven
 sudo apt install openjdk-8-jre maven default-jre -y
 
