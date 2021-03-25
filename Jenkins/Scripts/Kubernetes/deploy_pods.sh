a2c36757785b87a276f02385a85dfd3cf3c2244d
stegojulia <j.z.jablonska@gmail.com>
2021-03-25 22:43:47 +0000

    fixed path for deployment yaml

--- Jenkins/Scripts/Kubernetes/deploy_pods.sh
+++ Jenkins/Scripts/Kubernetes/deploy_pods.sh
@@ -3,7 +3,7 @@
 # ----- PETCLINIC -----
 
 # Runs our back end REST API. We must start this before the front end.
-envsubst < deployment.yaml | kubectl apply -f -
+envsubst < Kubernetes/deployment.yaml | kubectl apply -f -
 
 # Runs our front end Angular file.
 kubectl apply -f Kubernetes/deployment-client.yaml
