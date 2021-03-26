5c283b9a600438ca5c03190a5758e617b3a45347
stegojulia <j.z.jablonska@gmail.com>
2021-03-26 01:35:31 +0000

    sql test

--- Jenkins/Scripts/Kubernetes/deploy_pods.sh
+++ Jenkins/Scripts/Kubernetes/deploy_pods.sh
@@ -3,6 +3,9 @@
 # ----- PETCLINIC -----
 
 # Runs our back end REST API. We must start this before the front end.
+echo $RDS_ENDPOINT
+echo $RDS_USERNAME
+echo $RDS_PASSWORD
 envsubst < Kubernetes/deployment.yaml | kubectl apply -f -
 
 # Runs our front end Angular file.
