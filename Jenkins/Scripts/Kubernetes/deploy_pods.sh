a56f0f137abfa4166f48abca6f41a59b973996ab
stegojulia <j.z.jablonska@gmail.com>
2021-04-02 19:53:28 +0100

    cleaned up Kubernetes

--- Jenkins/Scripts/Kubernetes/deploy_pods.sh
+++ Jenkins/Scripts/Kubernetes/deploy_pods.sh
@@ -3,13 +3,8 @@
 # ----- PETCLINIC -----
 
 # Runs our back end REST API. We must start this before the front end.
-echo $RDS_ENDPOINT
-echo $RDS_USERNAME
-echo $RDS_PASSWORD
 envsubst < Kubernetes/deployment.yaml | kubectl apply -f -
 
 # Runs our front end Angular file.
 kubectl apply -f Kubernetes/deployment-client.yaml
 
-# Runs our loadBalancer service
-#kubectl apply -f Kubernetes/loadbalancer.yaml
